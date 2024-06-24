3359399221a48fe8dcf9d8356de14f9603ad74e0
Jin Park <jinpark1504@gmail.com>
2024-06-24 13:10:51 +0900

    Initial Commit

---
+++ WordsBrowser/WordsBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsBrowserApp.swift
+//  WordsBrowser
+//
+//  Created by Jungjin Park on 2024-06-24.
+//
+
+import SwiftUI
+
+@main
+struct WordsBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
